4f4044fa12becb62da03801512a9a82a0b5a999c
xiaoz <xiaoz@DESKTOP-L9E1RR1>
2021-05-20 12:52:08 -0400

    first commit

---
+++ lessons/src/Javalove.java
@@ -0,0 +1,11 @@
+
+public class Javalove {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int x = 1; x < 100; x+=3) {
+			System.out.println(x);
+		}
+	}
+
+}
